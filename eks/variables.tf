2032a219d16b7d40b9a7c34a8c7d799788bfed7b
gani-2024 <roneethsai@gmail.com>
2024-06-10 09:59:39 +0000

    commit_new

---
+++ eks/variables.tf
@@ -0,0 +1,14 @@
+variable "subnet_id_1" {
+  type = string
+  default = "subnet-03ac6fbbe08d230cf"
+ }
+
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-0f80f165c79a14c02"
+ }
+
+ variable "cluster_name" {
+  type = string
+  default = "my-eks-cluster"
+ }
